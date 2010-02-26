48d02c919e3a71da4b9b804bcb559003a4bc51c1
Wowka321 <galaxys@inbox.ru>
2010-02-26 17:41:20 +0200

    Proper caster for some visual spells

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3191,13 +3191,15 @@ void Spell::SendSpellStart()
     if(m_spellInfo->runeCostID)
         castFlags |= CAST_FLAG_UNKNOWN10;
 
+    Unit *caster = m_originalCaster ? m_originalCaster : m_caster;
+
     WorldPacket data(SMSG_SPELL_START, (8+8+4+4+2));
     if(m_CastItem)
         data.append(m_CastItem->GetPackGUID());
     else
-        data.append(m_caster->GetPackGUID());
+        data.append(caster->GetPackGUID());
 
-    data.append(m_caster->GetPackGUID());
+    data.append(caster->GetPackGUID());
     data << uint8(m_cast_count);                            // pending spell cast?
     data << uint32(m_spellInfo->Id);                        // spellId
     data << uint32(castFlags);                              // cast flags
@@ -3254,14 +3256,16 @@ void Spell::SendSpellGo()
         castFlags |= CAST_FLAG_UNKNOWN7;                    // rune cooldowns list
     }
 
+    Unit *caster = m_originalCaster ? m_originalCaster : m_caster;
+
     WorldPacket data(SMSG_SPELL_GO, 50);                    // guess size
 
     if(m_CastItem)
         data.append(m_CastItem->GetPackGUID());
     else
-        data.append(m_caster->GetPackGUID());
+        data.append(caster->GetPackGUID());
 
-    data.append(m_caster->GetPackGUID());
+    data.append(caster->GetPackGUID());
     data << uint8(m_cast_count);                            // pending spell cast?
     data << uint32(m_spellInfo->Id);                        // spellId
     data << uint32(castFlags);                              // cast flags
