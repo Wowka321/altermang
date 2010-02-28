93d19050c7e7b9ea6aaed59683f6fc3629f15e4b
Wowka321 <galaxys@inbox.ru>
2010-02-28 15:40:11 +0200

    Remove invis at spell cast. By zergtmn

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2582,6 +2582,7 @@ void Spell::prepare(SpellCastTargets const* targets, Aura* triggeredByAura)
     // add non-triggered (with cast time and without)
     if (!m_IsTriggeredSpell)
     {
+        m_caster->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_UNK11);
         // add to cast type slot
         m_caster->SetCurrentCastedSpell( this );
 
