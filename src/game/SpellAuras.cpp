7746ca5ce4558e57fbbb109aabcac1c3502d1c90
Wowka321 <galaxys@inbox.ru>
2010-02-28 19:49:42 +0200

    p63 Spell 58875

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4378,6 +4378,9 @@ void Aura::HandleAuraModIncreaseSpeed(bool /*apply*/, bool Real)
         return;
 
     m_target->UpdateSpeed(MOVE_RUN, true);
+
+    if (apply && GetSpellProto()->Id == 58875)
+        m_target->CastSpell(m_target, 58876, true);
 }
 
 void Aura::HandleAuraModIncreaseMountedSpeed(bool apply, bool Real)
