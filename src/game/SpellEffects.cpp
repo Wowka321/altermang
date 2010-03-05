5ff63fd9415d9ff66c403c0231a8ad64a71dbd23
Wowka321 <galaxys@inbox.ru>
2010-03-05 13:24:55 +0200

    Riptide Chain heal boost

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3275,7 +3275,19 @@ void Spell::EffectHeal(SpellEffectIndex /*eff_idx*/)
 
             addhealth += tickheal * tickcount;
         }
+        // Chain Heal consumes Riptide
+        else if(m_spellInfo->SpellFamilyName == SPELLFAMILY_SHAMAN && m_spellInfo->SpellFamilyFlags == UI64LIT(0x100))
+        {
+            addhealth = caster->SpellHealingBonus(unitTarget, m_spellInfo, addhealth, HEAL);
+
+            if (Aura *riptide = unitTarget->GetAura(SPELL_AURA_PERIODIC_HEAL, SPELLFAMILY_SHAMAN, 0, 0x10, m_caster->GetGUID()))
+            {
+                addhealth += addhealth * riptide->GetSpellProto()->EffectBasePoints[EFFECT_INDEX_2] / 100;
+                unitTarget->RemoveAurasDueToSpell(riptide->GetId());
+            }
+        }
         else
+
             addhealth = caster->SpellHealingBonus(unitTarget, m_spellInfo, addhealth, HEAL);
 
         m_healing+=addhealth;
