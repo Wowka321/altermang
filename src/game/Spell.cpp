0f5cb5b1cc17db9f10c74806fb18135026ff08e3
Wowka321 <galaxys@inbox.ru>
2010-03-08 21:02:51 +0200

    Revert

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3375,15 +3375,13 @@ void Spell::SendSpellStart()
     if (m_spellInfo->runeCostID)
         castFlags |= CAST_FLAG_UNKNOWN10;
 
-    Unit *caster = m_originalCaster ? m_originalCaster : m_caster;
-
     WorldPacket data(SMSG_SPELL_START, (8+8+4+4+2));
     if (m_CastItem)
         data.append(m_CastItem->GetPackGUID());
     else
-        data.append(caster->GetPackGUID());
+        data.append(m_caster->GetPackGUID());
 
-    data.append(caster->GetPackGUID());
+    data.append(m_caster->GetPackGUID());
     data << uint8(m_cast_count);                            // pending spell cast?
     data << uint32(m_spellInfo->Id);                        // spellId
     data << uint32(castFlags);                              // cast flags
@@ -3440,16 +3438,14 @@ void Spell::SendSpellGo()
         castFlags |= CAST_FLAG_UNKNOWN7;                    // rune cooldowns list
     }
 
-    Unit *caster = m_originalCaster ? m_originalCaster : m_caster;
-
     WorldPacket data(SMSG_SPELL_GO, 50);                    // guess size
 
     if(m_CastItem)
         data.append(m_CastItem->GetPackGUID());
     else
-        data.append(caster->GetPackGUID());
+        data.append(m_caster->GetPackGUID());
 
-    data.append(caster->GetPackGUID());
+    data.append(m_caster->GetPackGUID());
     data << uint8(m_cast_count);                            // pending spell cast?
     data << uint32(m_spellInfo->Id);                        // spellId
     data << uint32(castFlags);                              // cast flags
