0ad04f6cff0d255d3835eaaed124915c2e03eb85
Wowka321 <galaxys@inbox.ru>
2010-03-10 00:15:07 +0200

    Fix gain XP and reward system

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -15739,6 +15739,9 @@ void Player::_LoadJail(void)
 
 bool Player::isAllowedToLoot(Creature* creature)
 {
+    if(creature && creature->isDead() && !creature->AreLootAndRewardAllowed())
+        return false;
+
     if(Player* recipient = creature->GetLootRecipient())
     {
         if (recipient == this)
