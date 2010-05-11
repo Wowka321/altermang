058425be3c3c9e1daa9cafb08a86ee763d4e1097
gmltA <gmltt@yandex.ru>
2010-05-11 04:10:59 -0700

    Solace of the Defeated Heroic & Normal versions stack

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1470,6 +1470,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                     if( spellInfo_1->SpellIconID == 3559 && spellInfo_2->SpellIconID == 3559 )
                         return false;
 
+                    //Solace of the Defeated Heroic & Normal versions
+                    if( (spellInfo_1->Id == 67696 && spellInfo_2->Id == 67750) ||
+                        (spellInfo_2->Id == 67696 && spellInfo_1->Id == 67750) )
+                        return false;
+
                     break;
                 }
                 case SPELLFAMILY_MAGE:
