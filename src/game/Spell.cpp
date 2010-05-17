374f02c12addd8e296d58cbfe68227bd7e7f87ea
Wowka321 <galaxys@inbox.ru>
2010-05-17 10:26:12 +0300

    Lay of Hands should aplly Forberance

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2948,6 +2948,15 @@ void Spell::cast(bool skipCheck)
             }
             else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x200000000000))
                 AddPrecastSpell(61987);                     // Avenging Wrath Marker
+            // Lay on Hands
+            else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x000000008000))
+            {
+                if (m_targets.getUnitTarget() && m_targets.getUnitTarget() == m_caster)
+                {
+                    AddPrecastSpell(25771);                 // Forbearance
+                    AddPrecastSpell(61987);                 // Avenging Wrath Marker
+                }
+            }
             break;
         }
         case SPELLFAMILY_SHAMAN:
@@ -4418,6 +4427,8 @@ SpellCastResult Spell::CheckCast(bool strict)
             else if (target->HasAura(m_spellInfo->excludeTargetAuraSpell))
                 return SPELL_FAILED_CASTER_AURASTATE;
         }
+        else if((m_spellInfo->SpellFamilyFlags & UI64LIT(0x000000008000)) && (target->HasAura(25771) || target->HasAura(61987)))
+            return SPELL_FAILED_CASTER_AURASTATE;
 
         bool non_caster_target = target != m_caster && !IsSpellWithCasterSourceTargetsOnly(m_spellInfo);
 
