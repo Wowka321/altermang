01b838db0aa9dac7d7e6952b8c7756ca77c2f156
Wowka321 <galaxys@inbox.ru>
2010-05-17 10:36:50 +0300

    Vigilance, by selector.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6139,6 +6139,15 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     // Torture the Torturer: High Executor's Branding Iron Impact
                     unitTarget->CastSpell(unitTarget, 48614, true);
                     return;
+                case 50725:                                 // Vigilance
+                {
+                    if (!unitTarget || unitTarget->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    // Remove Taunt cooldown
+                    ((Player*)unitTarget)->RemoveSpellCooldown(355, true);
+                    return;
+                }
                 case 51770:                                 // Emblazon Runeblade
                 {
                     Unit* caster = GetAffectiveCaster();
