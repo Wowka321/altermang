74526599cf2bd1bf6e2717b42db899d3f6685edf
Wowka321 <galaxys@inbox.ru>
2010-05-31 21:24:41 +0300

    Hackly fix some WOTLK commendation badges.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7706,6 +7706,9 @@ void Spell::EffectReputation(SpellEffectIndex eff_idx)
 
     int32  rep_change = m_currentBasePoints[eff_idx];
 
+    if (rep_change == 400) //There are should be some factions flag???
+        rep_change *= 1.3;
+
     uint32 faction_id = m_spellInfo->EffectMiscValue[eff_idx];
 
     FactionEntry const* factionEntry = sFactionStore.LookupEntry(faction_id);
