636e26fe747b70ef24ad3d0ab5a6f85e1ece8a83
PSZ <PSZ@getmangos.com>
2010-06-02 13:40:33 +0300

    p101. Fix player pull

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7952,11 +7952,7 @@ void Spell::EffectPlayerPull(SpellEffectIndex eff_idx)
     if(!unitTarget)
         return;
 
-    float dist = unitTarget->GetDistance2d(m_caster);
-    if (damage && dist > damage)
-        dist = float(damage);
-
-    unitTarget->KnockBackFrom(m_caster,-dist,float(m_spellInfo->EffectMiscValue[eff_idx])/30);
+    unitTarget->KnockBackFrom(m_caster, -unitTarget->GetDistance2d(m_caster), float(m_spellInfo->EffectMiscValue[eff_idx])/30);
 }
 
 void Spell::EffectDispelMechanic(SpellEffectIndex eff_idx)
