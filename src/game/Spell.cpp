e0fd2da98f36c04bc58ba920b65aac7dc830938c
Laise <fenrisse@gmail.com>
2010-06-06 21:20:17 +0300

    p194.New version

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4194,7 +4194,10 @@ SpellCastResult Spell::CheckOrTakeRunePower(bool take)
                 --runeCost[rune];
 
                 if (take)
+                {
                     plr->ConvertRune(i, plr->GetBaseRune(i));
+                    plr->ClearConvertedBy(i);
+                }
             }
         }
 
