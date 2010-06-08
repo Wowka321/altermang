f24ab1741a9a91f102de32a741fae719954c1218
Wowka321 <galaxys@inbox.ru>
2010-06-08 20:22:12 +0300

    Fix removing debuf 70541 at 90% health.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -7506,6 +7506,15 @@ void Aura::PeriodicTick()
                         }
                         break;
                     }
+                    case 70541:
+                    {
+                        if(target->GetHealth() >= target->GetMaxHealth() * 0.9 )
+                        {
+                            target->RemoveAurasDueToSpell(GetId());
+                            return;
+                        }
+                        break;
+                    }
                     default:
                         break;
                 }
