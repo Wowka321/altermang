124ce7250243fd27ee8baa8cfa9b7bf67b2a3884
gmltA <gmltt@yandex.ru>
2010-06-14 18:24:04 +0400

    Implent spells 71519 and 71562 and...
    *Deathbringer's Will heroic and normal variants
    *Third string of sql file affects on spell 55666 [must proc from Scourges Strike]
    
    Signed-off-by: gmltA <gmltt@yandex.ru>

---
+++ addition/spell_proc_event_important.sql
@@ -0,0 +1,3 @@
+INSERT INTO `spell_proc_event` VALUES ('71562', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
+INSERT INTO `spell_proc_event` VALUES ('71519', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
+UPDATE `mangos`.`spell_proc_event` SET `SpellFamilyMask1` = 134217728 WHERE `entry` = 55666;
