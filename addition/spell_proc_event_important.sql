e8f7aa42e470b20ac8b81cf42065c09e1a50f361
Wowka321 <galaxys@inbox.ru>
2010-06-14 18:59:08 +0300

    Just for beauty ;)

--- addition/spell_proc_event_important.sql
+++ addition/spell_proc_event_important.sql
@@ -1,3 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (71562, 71519);
 INSERT INTO `spell_proc_event` VALUES ('71562', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
 INSERT INTO `spell_proc_event` VALUES ('71519', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
-UPDATE `mangos`.`spell_proc_event` SET `SpellFamilyMask1` = 134217728 WHERE `entry` = 55666;
+UPDATE `spell_proc_event` SET `SpellFamilyMask1` =  0x08000000 WHERE `entry` = 55666;
