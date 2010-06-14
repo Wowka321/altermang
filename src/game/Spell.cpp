e487fd6b22ef70a034dcd90b014f074a7cf9f9ca
Wowka321 <galaxys@inbox.ru>
2010-06-14 23:41:35 +0300

    Revert. This broke some ressurect spells, also maybe strange random crashes ((

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3286,9 +3286,6 @@ void Spell::update(uint32 difftime)
         {
             if(m_timer)
             {
-                if (m_targets.getUnitTarget() && !m_targets.getUnitTarget()->isVisibleForOrDetect(m_caster, m_caster, false))
-                    cancel();
-
                 if(difftime >= m_timer)
                     m_timer = 0;
                 else
