1c22e52e2609d5e935b7f8df6667de1d96511df1
gmltA <gmltt@yandex.ru>
2010-07-01 08:29:35 +0300

    Flameshock periodic damage now can crit
    Signed-off-by: gmltA <gmltt@yandex.ru>

---
+++ addition/[add_3]playercreateinfo_spell.sql
@@ -0,0 +1,6 @@
+/* Flameshock tick crit */
+INSERT IGNORE INTO playercreateinfo_spell VALUES
+(2,  7, 75461, 'Flame Shock Passive'),
+(6,  7, 75461, 'Flame Shock Passive'),
+(8,  7, 75461, 'Flame Shock Passive'),
+(11, 7, 75461, 'Flame Shock Passive');
